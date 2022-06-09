7d8a80e5d69771b7c0f43c3acbd7cb9896822a63
Jakob Didio <jakobdidio@gmail.com>
2022-06-08 22:10:32 -0700

    Integrated statistics pickling with synonym attack.

---
+++ run_compstat_attack.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source ../env/bin/activate
+python whitebox_attack_compstats.py --dataset imdb --data_folder ./data/ --batch_size 1 --num_samples 80 --finetune True --gumbel_samples 100 --start_index 100
